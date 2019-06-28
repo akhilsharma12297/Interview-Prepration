32e0857d94954a770875df2d5f5b62730b2ef0bf
akhilsharma12297 <akhil.sharma12297@gmail.com>
2019-06-28 19:32:41 +0530

    Code

---
+++ Arrays_String/nesting_array.java
@@ -0,0 +1,50 @@
+package Arrays_String;
+
+import java.util.HashSet;
+import java.util.LinkedList;
+
+public class nesting_array {
+
+	public static void main(String[] args) {
+
+		int[] arr = { 4, 3, 3, 4, 1, 0, 5 };
+		func(arr);
+	}
+
+	private static void func(int[] arr) {
+
+		HashSet<Integer> visited = new HashSet<Integer>();
+
+		LinkedList<Integer> queue = new LinkedList<Integer>();
+
+		queue.add(arr[0]);
+
+		visited.add(arr[0]);
+
+		int ctr = 1;
+		int max = 0;
+		int i = 1;
+		while (i < arr.length) {
+
+			if (queue.size() != 0) {
+				int temp = queue.remove();
+				if (!visited.contains(arr[temp])) {
+					queue.add(arr[temp]);
+					visited.add(arr[temp]);
+					ctr++;
+				} else {
+
+					max = Math.max(max, ctr);
+				}
+			} else {
+				queue.add(arr[i]);
+				ctr = 0;
+				i++;
+			}
+
+		}
+
+		System.out.println(max);
+
+	}
+}
