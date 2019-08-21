5d2fbc5b092dbfa02327ae238713045674b0547c
akhilsharma12297 <akhil.sharma12297@gmail.com>
2019-08-21 09:21:14 +0530

    Code

--- dp/CountwaystoexpressNasSumof134.java
+++ dp/CountwaystoexpressNasSumof134.java
@@ -1,20 +1,18 @@
+package dp;
 
 public class CountwaystoexpressNasSumof134 {
-static int countWays(int n) 
-		{ 
-			int DP[] = new int[n + 1]; 
+	static int countWays(int n) {
+		int DP[] = new int[n + 1];
 
-			// base cases 
-			DP[0] = DP[1] = DP[2] = 1; 
-			DP[3] = 2; 
+		// base cases
+		DP[0] = DP[1] = DP[2] = 1;
+		DP[3] = 2;
 
-			// iterate for all values from 4 to n 
-			for (int i = 4; i <= n; i++) 
-				DP[i] = DP[i - 1] + DP[i - 3] 
-						+ DP[i - 4]; 
+		// iterate for all values from 4 to n
+		for (int i = 4; i <= n; i++)
+			DP[i] = DP[i - 1] + DP[i - 3] + DP[i - 4];
 
-			return DP[n]; 
-		} 
-
+		return DP[n];
+	}
 
 }
